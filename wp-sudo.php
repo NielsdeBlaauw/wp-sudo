f83afcfc06cb9b965dc5c531df10e387e971f965
Niels de Blaauw <niels@level-level.com>
2018-05-16 09:02:13 +0000

    Adds check for default admin username

--- wp-sudo.php
+++ wp-sudo.php
@@ -1,13 +1,37 @@
 <?php
 
+namespace WPSudo;
+
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
 add_action('admin_notices', function(){
-    wp_die();
-    $class = 'notice notice-error';
-    $title = __("Users with admin roles are public");
-	$message = __( 'There are users that have a administration permission that have published public posts. This means their user information is publicly available.', 'sample-text-domain' );
-
-	printf( '<div class="%1$s"><h2>%2$s</2><p>%3$s</p></div>', esc_attr( $class ), esc_html($title), esc_html( $message ) ); 
+    check_admin_default_username();
+    check_admin_users_public();
 });
+
+function check_admin_users_public(){
+    warn_admin_users_public();
+}
+
+function warn_admin_users_public(){
+    $class = 'notice notice-error';
+    $title = __('Security: Admin user information is publicly available', 'wp-sudo');
+	$message = __( 'There seem to be admin users that have published public posts. Information about users that have public posts is freely available on your website. This information appears in sitemaps, the WordPress API and other places. Hackers and bots can use this information to attack your website.', 'wp-sudo' );
+
+	printf( '<div class="%1$s"><h1>%2$s</h1><p>%3$s</p></div>', esc_attr( $class ), esc_html($title), esc_html( $message ) ); 
+}
+
+function check_admin_default_username(){
+    if(get_user_by('slug', 'admin')){
+        warn_admin_default_username();
+    }
+}
+
+function warn_admin_default_username(){
+    $class = 'notice notice-error';
+    $title = __('Security: Using default admin username', 'wp-sudo');
+	$message = __( 'This webste has a user with the default \'admin\' username. This username is often tested by automated tools in an attempt to hack your website.', 'wp-sudo' );
+
+	printf( '<div class="%1$s"><h1>%2$s</h1><p>%3$s</p></div>', esc_attr( $class ), esc_html($title), esc_html( $message ) ); 
+}
